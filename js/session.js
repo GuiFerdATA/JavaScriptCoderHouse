5d6e900cfa5137d55ad15922ccad0d27ff60755c
Guille Fernandez <guille.fer79@hotmail.com>
2022-08-11 21:32:51 -0300

    cambiamos texto de alert

--- js/session.js
+++ js/session.js
@@ -2,7 +2,7 @@ function ir() {
     let c = 1234;
     let u = "JavaScript";
     if (document.form.password.value == c && document.form.login.value == u) {
-        alert("Bienvenidos");
+        alert("¡Usuario y contraseña correctos!");
         window.location = "promedio.html";
     }
     else {
